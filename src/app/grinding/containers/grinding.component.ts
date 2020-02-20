49e0cc2d80f191b2dd17a592c1e1c94aee338cd2
ha <typ0@naver.com>
2020-02-21 00:44:51 +0900

    feat: player level

--- src/app/grinding/containers/grinding.component.ts
+++ src/app/grinding/containers/grinding.component.ts
@@ -40,6 +40,7 @@ export class GrindingComponent implements OnInit {
   ];
 
   expBuff = 0;
+  playerLevel = 250;
 
   constructor() {}
 
@@ -47,8 +48,13 @@ export class GrindingComponent implements OnInit {
     this.list = new MatTableDataSource(
       maps.map(map => {
         const countPerHour = (map.count * 60 * 60) / 7.5;
-        const expPerHour = countPerHour * map.exp;
-        const mesoPerHour = countPerHour * map.level * 7.5;
+        const expPerHour =
+          (countPerHour * map.mobs.reduce((acc, mob) => acc + mob.exp, 0)) /
+          map.mobs.length;
+        const mesoPerHour =
+          ((countPerHour * map.mobs.reduce((acc, mob) => acc + mob.level, 0)) /
+            map.mobs.length) *
+          7.5;
         const backgroundColor = backgroundColors[map.group];
 
         return {
@@ -64,26 +70,92 @@ export class GrindingComponent implements OnInit {
     this.list.sort = this.sort;
   }
 
-  private getExpPerHour(data: MapView, expBuff: number) {
+  private getExpPerHour(data: MapView, expBuff: number, playerLevel: number) {
     return (
-      data.countPerHour * data.exp * (1 + expBuff / 100 + data.burning / 10)
+      data.mobs.reduce(
+        (acc, mob) =>
+          acc +
+          data.countPerHour *
+            mob.exp *
+            (1 + expBuff / 100 + data.burning / 10) *
+            this.getLevelModifier(mob.level, playerLevel),
+        0,
+      ) / data.mobs.length
     );
   }
 
-  trackByName(data: MapView) {
+  private getLevelModifier(mobLevel: number, playerLevel: number) {
+    if (playerLevel - mobLevel >= 40) {
+      return 0.7;
+    }
+    if (playerLevel - mobLevel >= 21) {
+      return 0.9 - (playerLevel - mobLevel - 20) / 100;
+    }
+    if (playerLevel - mobLevel >= 19) {
+      return 0.95;
+    }
+    if (playerLevel - mobLevel >= 17) {
+      return 0.96;
+    }
+    if (playerLevel - mobLevel >= 15) {
+      return 0.97;
+    }
+    if (playerLevel - mobLevel >= 13) {
+      return 0.98;
+    }
+    if (playerLevel - mobLevel >= 11) {
+      return 0.99;
+    }
+    if (playerLevel - mobLevel === 10) {
+      return 1;
+    }
+    if (playerLevel - mobLevel >= 5) {
+      return 1.05;
+    }
+    if (playerLevel - mobLevel >= -4) {
+      return 1.1;
+    }
+    if (playerLevel - mobLevel >= -9) {
+      return 1.05;
+    }
+    if (playerLevel - mobLevel >= -20) {
+      return 1 + (playerLevel - mobLevel + 10) / 100;
+    }
+    if (playerLevel - mobLevel >= -35) {
+      return 0.7 + ((playerLevel - mobLevel + 21) / 100) * 4;
+    }
+    if (playerLevel - mobLevel >= -39) {
+      return 0.1;
+    }
+    return 0;
+  }
+
+  trackByName(index: number, data: MapView) {
     return data.name;
   }
 
   setBurning(data: MapView, value: number) {
     data.burning = value;
-    data.expPerHour = this.getExpPerHour(data, this.expBuff);
+    data.expPerHour = this.getExpPerHour(data, this.expBuff, this.playerLevel);
+  }
+
+  setPlayerLevel(value: number) {
+    this.playerLevel = value;
+    this.list.data = this.list.data.map(x => ({
+      ...x,
+      expPerHour: this.getExpPerHour(x, this.expBuff, this.playerLevel),
+    }));
   }
 
   setExpBuff(buff: number) {
     this.expBuff = buff;
     this.list.data = this.list.data.map(x => ({
       ...x,
-      expPerHour: this.getExpPerHour(x, buff),
+      expPerHour: this.getExpPerHour(x, buff, this.playerLevel),
     }));
+  }
+
+  onBlur() {
+    this.list.data = this.list.data.slice();
   }
 }
