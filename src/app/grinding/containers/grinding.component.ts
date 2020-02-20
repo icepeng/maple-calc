aa8d9160b778d8f5048b5914fe7f271e21ba435d
ha <typ0@naver.com>
2020-02-21 01:18:47 +0900

    fix: meso modifier

--- src/app/grinding/containers/grinding.component.ts
+++ src/app/grinding/containers/grinding.component.ts
@@ -40,7 +40,7 @@ export class GrindingComponent implements OnInit {
   ];
 
   expBuff = 0;
-  playerLevel = 250;
+  playerLevel = null;
 
   constructor() {}
 
@@ -78,54 +78,116 @@ export class GrindingComponent implements OnInit {
           data.countPerHour *
             mob.exp *
             (1 + expBuff / 100 + data.burning / 10) *
-            this.getLevelModifier(mob.level, playerLevel),
+            this.getLevelExpModifier(mob.level, playerLevel),
         0,
       ) / data.mobs.length
     );
   }
 
-  private getLevelModifier(mobLevel: number, playerLevel: number) {
-    if (playerLevel - mobLevel >= 40) {
+  private getLevelExpModifier(mobLevel: number, playerLevel: number) {
+    const diff = playerLevel - mobLevel;
+    if (diff >= 40) {
       return 0.7;
     }
-    if (playerLevel - mobLevel >= 21) {
-      return 0.9 - (playerLevel - mobLevel - 20) / 100;
+    if (diff >= 21) {
+      return 0.9 - (diff - 20) / 100;
     }
-    if (playerLevel - mobLevel >= 19) {
+    if (diff >= 19) {
       return 0.95;
     }
-    if (playerLevel - mobLevel >= 17) {
+    if (diff >= 17) {
       return 0.96;
     }
-    if (playerLevel - mobLevel >= 15) {
+    if (diff >= 15) {
       return 0.97;
     }
-    if (playerLevel - mobLevel >= 13) {
+    if (diff >= 13) {
       return 0.98;
     }
-    if (playerLevel - mobLevel >= 11) {
+    if (diff >= 11) {
       return 0.99;
     }
-    if (playerLevel - mobLevel === 10) {
+    if (diff >= 10) {
       return 1;
     }
-    if (playerLevel - mobLevel >= 5) {
+    if (diff >= 5) {
       return 1.05;
     }
-    if (playerLevel - mobLevel >= -4) {
+    if (diff >= -4) {
       return 1.1;
     }
-    if (playerLevel - mobLevel >= -9) {
+    if (diff >= -9) {
       return 1.05;
     }
-    if (playerLevel - mobLevel >= -20) {
-      return 1 + (playerLevel - mobLevel + 10) / 100;
+    if (diff >= -20) {
+      return 1 + (diff + 10) / 100;
     }
-    if (playerLevel - mobLevel >= -35) {
-      return 0.7 + ((playerLevel - mobLevel + 21) / 100) * 4;
+    if (diff >= -35) {
+      return 0.7 + ((diff + 21) / 100) * 4;
     }
-    if (playerLevel - mobLevel >= -39) {
+    if (diff >= -39) {
       return 0.1;
+    }
+    return 0;
+  }
+
+  private getMesoPerHour(data: MapView, playerLevel: number) {
+    return (
+      data.mobs.reduce(
+        (acc, mob) =>
+          acc +
+          data.countPerHour *
+            mob.level *
+            7.5 *
+            this.getLevelMesoModifier(mob.level, playerLevel),
+        0,
+      ) / data.mobs.length
+    );
+  }
+
+  private getLevelMesoModifier(mobLevel: number, playerLevel: number) {
+    const diff = playerLevel - mobLevel;
+    if (diff >= 30) {
+      return 0;
+    }
+    if (diff >= 29) {
+      return 0.03;
+    }
+    if (diff >= 28) {
+      return 0.16;
+    }
+    if (diff >= 27) {
+      return 0.24;
+    }
+    if (diff >= 26) {
+      return 0.35;
+    }
+    if (diff >= 25) {
+      return 0.45;
+    }
+    if (diff >= 24) {
+      return 0.54;
+    }
+    if (diff >= 23) {
+      return 0.62;
+    }
+    if (diff >= 22) {
+      return 0.69;
+    }
+    if (diff >= 21) {
+      return 0.75;
+    }
+    if (diff >= 11) {
+      return 1 - ((diff - 10) / 100) * 2;
+    }
+    if (diff >= -10) {
+      return 1;
+    }
+    if (diff >= -20) {
+      return 1 + ((diff + 10) / 100) * 3;
+    }
+    if (diff >= -33) {
+      return 0.7 + ((diff + 20) / 100) * 5;
     }
     return 0;
   }
@@ -143,7 +205,8 @@ export class GrindingComponent implements OnInit {
     this.playerLevel = value;
     this.list.data = this.list.data.map(x => ({
       ...x,
-      expPerHour: this.getExpPerHour(x, this.expBuff, this.playerLevel),
+      expPerHour: this.getExpPerHour(x, this.expBuff, value),
+      mesoPerHour: this.getMesoPerHour(x, value),
     }));
   }
 
