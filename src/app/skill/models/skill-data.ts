39879761050f1bb51b79ec9b77caa9d68d6fc41c
ha <typ0@naver.com>
2021-04-29 19:33:38 +0900

    feat: skill data

--- src/app/skill/models/skill-data.ts
+++ src/app/skill/models/skill-data.ts
@@ -2669,6 +2669,23 @@ export const chtrSkillData: SkillData[] = [
     },
   },
   {
+    name: '호밍 미사일',
+    location: 'assets/homingmissile.png',
+    job: '메카닉',
+    lt: {
+      x: -500,
+      y: -300,
+    },
+    rb: {
+      x: 0,
+      y: 0,
+    },
+    origin: {
+      x: 14,
+      y: 112,
+    },
+  },
+  {
     name: '디스토션 필드',
     location: 'assets/distortionfield.png',
     job: '메카닉',
@@ -2683,6 +2700,23 @@ export const chtrSkillData: SkillData[] = [
     origin: {
       x: 457,
       y: 213,
+    },
+  },
+  {
+    name: '마이크로 미사일 컨테이너',
+    location: 'assets/micromissilecontainer.png',
+    job: '메카닉',
+    lt: {
+      x: -800,
+      y: -500,
+    },
+    rb: {
+      x: 800,
+      y: 500,
+    },
+    origin: {
+      x: 533,
+      y: 486,
     },
   },
   {
