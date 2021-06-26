e5f63a48f333e9cf7b2d20c074ae1050b19d1eb4
ha <typ0@naver.com>
2021-06-26 22:11:37 +0900

    feat: kanna skill data

--- src/app/skill/models/skill-data.ts
+++ src/app/skill/models/skill-data.ts
@@ -425,6 +425,23 @@ export const bossSkillData: SkillData[] = [
 
 export const chtrSkillData: SkillData[] = [
   {
+    name: '악몽으로의 초대',
+    location: 'assets/lucidsoul.png',
+    job: '공용',
+    lt: {
+      x: -400,
+      y: -475,
+    },
+    rb: {
+      x: 400,
+      y: 125,
+    },
+    origin: {
+      x: 284,
+      y: 395,
+    },
+  },
+  {
     name: '파이렛 플래그',
     location: 'assets/pirateflag.png',
     job: '공용',
@@ -6501,6 +6518,296 @@ export const chtrSkillData: SkillData[] = [
       x: 1327,
       y: 441,
     },
+  },
+  {
+    name: '마이크로 블랙홀',
+    location: 'assets/microblackhole.png',
+    job: '키네시스',
+    lt: {
+      x: -900,
+      y: -160,
+    },
+    rb: {
+      x: -630,
+      y: 110,
+    },
+    origin: {
+      x: 1327,
+      y: 441,
+    },
+  },
+  {
+    name: 'Shikigami Haunting 4',
+    location: 'assets/shikigami.png',
+    job: '칸나',
+    lt: {
+      x: -475,
+      y: -190,
+    },
+    rb: {
+      x: 0,
+      y: 30,
+    },
+    origin: {
+      x: 499,
+      y: 233,
+    },
+  },
+  {
+    name: 'Ghost Yaksha Boss (1)',
+    location: 'assets/ghost_yaksha_1.png',
+    job: '칸나',
+    lt: {
+      x: -205,
+      y: -325,
+    },
+    rb: {
+      x: 205,
+      y: 45,
+    },
+    origin: {
+      x: 260,
+      y: 550,
+    },
+  },
+  {
+    name: 'Ghost Yaksha Boss (2)',
+    location: 'assets/ghost_yaksha_2.png',
+    job: '칸나',
+    lt: {
+      x: -470,
+      y: -350,
+    },
+    rb: {
+      x: 180,
+      y: 40,
+    },
+    origin: {
+      x: 695,
+      y: 582,
+    },
+  },
+  {
+    name: 'Ghost Yaksha Boss (3)',
+    location: 'assets/ghost_yaksha_3.png',
+    job: '칸나',
+    lt: {
+      x: -600,
+      y: -350,
+    },
+    rb: {
+      x: 180,
+      y: 20,
+    },
+    origin: {
+      x: 1089,
+      y: 697,
+    },
+  },
+  {
+    name: 'Ghost Yaksha Boss (4)',
+    location: 'assets/ghost_yaksha_4.png',
+    job: '칸나',
+    lt: {
+      x: -665,
+      y: -350,
+    },
+    rb: {
+      x: 175,
+      y: 45,
+    },
+    origin: {
+      x: 928,
+      y: 588,
+    },
+  },
+  {
+    name: 'Nine-Tailed Fury',
+    location: 'assets/nine_tailed_fury.png',
+    job: '칸나',
+    lt: {
+      x: -600,
+      y: -600,
+    },
+    rb: {
+      x: 600,
+      y: 400,
+    },
+    origin: {
+      x: 629,
+      y: 323,
+    },
+  },
+  {
+    name: 'Shikigami Doppleganger',
+    location: 'assets/shikigami_doppelganger.png',
+    job: '칸나',
+    lt: {
+      x: -512,
+      y: -150,
+    },
+    rb: {
+      x: 512,
+      y: 20,
+    },
+    origin: {
+      x: 568,
+      y: 282,
+    },
+  },
+  {
+    name: 'Orochi Unbound',
+    location: 'assets/orochi_unbound.png',
+    job: '칸나',
+    lt: {
+      x: -512,
+      y: -350,
+    },
+    rb: {
+      x: 512,
+      y: 350,
+    },
+    origin: {
+      x: 555,
+      y: 858,
+    },
+  },
+  {
+    name: "Vanquisher's Charm",
+    location: 'assets/vanquishers_charm.png',
+    job: '칸나',
+    lt: {
+      x: -544,
+      y: -235,
+    },
+    rb: {
+      x: 7,
+      y: 50,
+    },
+    origin: {
+      x: 582,
+      y: 257,
+    },
+  },
+  {
+    name: 'Falling Sakura',
+    location: 'assets/falling_sakura.png',
+    job: '칸나',
+    lt: {
+      x: -400,
+      y: -100,
+    },
+    rb: {
+      x: 0,
+      y: 70,
+    },
+    origin: {
+      x: 228 + 400,
+      y: 325 - 70,
+    },
+  },
+  {
+    name: 'Binding Tempest',
+    location: 'assets/binding_tempest.png',
+    job: '칸나',
+    lt: {
+      x: -745,
+      y: -290,
+    },
+    rb: {
+      x: 0,
+      y: 0,
+    },
+    origin: {
+      x: 765,
+      y: 308,
+    },
+  },
+  {
+    name: 'Circle of Suppression',
+    location: 'assets/circle_of_suppression.png',
+    job: '칸나',
+    lt: {
+      x: -300,
+      y: -150,
+    },
+    rb: {
+      x: 300,
+      y: 150,
+    },
+    origin: {
+      x: 72,
+      y: 126,
+    },
+  },
+  {
+    name: 'Veritable Pandemonium',
+    location: 'assets/veritable_pandemonium.png',
+    job: '칸나',
+    lt: {
+      x: -700,
+      y: -300,
+    },
+    rb: {
+      x: 700,
+      y: 300,
+    },
+    origin: {
+      x: 706,
+      y: 743,
+    },
+  },
+  {
+    name: 'Yuki-musume Shoukan (1)',
+    location: 'assets/yuki_musume_shoukan_1.png',
+    job: '칸나',
+    lt: {
+      x: -320,
+      y: -220,
+    },
+    rb: {
+      x: 0,
+      y: 30,
+    },
+    origin: {
+      x: 330,
+      y: 329,
+    },
+  },
+  {
+    name: 'Yuki-musume Shoukan (2)',
+    location: 'assets/yuki_musume_shoukan_2.png',
+    job: '칸나',
+    lt: {
+      x: -420,
+      y: -200,
+    },
+    rb: {
+      x: 0,
+      y: 30,
+    },
+    origin: {
+      x: 564,
+      y: 293,
+    },
+  },
+  {
+    name: 'Ghost Yaksha: Great Oni Lord\'s Legion',
+    location: 'assets/ghost_yaksha_great_oni.png',
+    job: '칸나',
+    lt: {
+      x: -1000,
+      y: -700,
+    },
+    rb: {
+      x: 1000,
+      y: 700,
+    },
+    origin: {
+      x: 391,
+      y: 242,
+    },
+    scale: 200,
   },
 ];
 
